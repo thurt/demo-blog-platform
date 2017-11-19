7ee5d6210d384d2ae85fa9ceb09571475e6c8fbe
thurt <taylor.a.hurt@gmail.com>
2017-11-19 16:42:01 +0000

    creates basic grpc-mysql implementations for all services

--- cms/server/server.go
+++ cms/server/server.go
@@ -36,11 +36,10 @@ func newServer() *cmsServer {
 	return s
 }
 
-func (s *cmsServer) GetPage(ctx context.Context, pid *pb.PageId) (*pb.Page, error) {
-	thisPage := &pb.Page{}
-	// TODO: validate inputs
+func (s *cmsServer) GetPost(ctx context.Context, r *pb.PostRequest) (*pb.Post, error) {
+	p := &pb.Post{}
 
-	err := database.QueryRow("SELECT page_title, page_content, page_date, page_guid FROM pages WHERE page_guid=?", pid.GetGuid()).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Created, &thisPage.Guid)
+	err := database.QueryRow("SELECT page_title, page_content, page_date, page_guid FROM pages WHERE page_guid=?", r.GetId()).Scan(&p.Title, &p.Content, &p.Created, &p.Id)
 
 	// TODO: return proper errors depending on results of previous code
 	if err != nil {
@@ -48,15 +47,14 @@ func (s *cmsServer) GetPage(ctx context.Context, pid *pb.PageId) (*pb.Page, erro
 		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
 	}
 
-	return thisPage, nil
+	return p, nil
 }
 
-func (s *cmsServer) CreatePage(ctx context.Context, cpr *pb.CreatePageRequest) (*pb.PageId, error) {
-	// TODO: validate inputs
+func (s *cmsServer) CreatePost(ctx context.Context, r *pb.CreatePostRequest) (*pb.PostRequest, error) {
 	// TODO: create a scheme to create a page_guid from the page_title (currently using hardCodedValue)
 	hardCodedValue := "hard-coded"
 
-	_, err := database.Exec("INSERT INTO pages SET page_guid=?, page_title=?, page_content=?", hardCodedValue, cpr.GetTitle(), cpr.GetContent())
+	_, err := database.Exec("INSERT INTO pages SET page_guid=?, page_title=?, page_content=?", hardCodedValue, r.GetTitle(), r.GetContent())
 
 	// TODO: return proper errors depending on the results of previous code (ie. sql row already exists, invalid inputs)
 	if err != nil {
@@ -64,12 +62,12 @@ func (s *cmsServer) CreatePage(ctx context.Context, cpr *pb.CreatePageRequest) (
 		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
 	}
 
-	return &pb.PageId{hardCodedValue}, nil
+	return &pb.PostRequest{hardCodedValue}, nil
 }
 
-func (s *cmsServer) DeletePage(ctx context.Context, pid *pb.PageId) (*empty.Empty, error) {
+func (s *cmsServer) DeletePost(ctx context.Context, pr *pb.PostRequest) (*empty.Empty, error) {
 	// TODO: validate inputs
-	_, err := database.Exec("DELETE FROM pages WHERE page_guid=?", pid.GetGuid())
+	_, err := database.Exec("DELETE FROM pages WHERE page_guid=?", pr.GetId())
 
 	// TODO: return proper errors depending on results of previous code
 	if err != nil {
@@ -79,33 +77,237 @@ func (s *cmsServer) DeletePage(ctx context.Context, pid *pb.PageId) (*empty.Empt
 	return &empty.Empty{}, nil
 }
 
-func (s *cmsServer) GetPageComments(ctx context.Context, pid *pb.PageId) (*pb.Comments, error) {
-	// TODO: validate inputs
-
-	comments, err := database.Query("SELECT comment_name,comment_text,comment_date FROM comments WHERE page_guid=?", pid.GetGuid())
+func (s *cmsServer) GetPostComments(r *pb.PostRequest, stream pb.Cms_GetPostCommentsServer) error {
+	cs, err := database.Query("SELECT id, content, created, last_edited, user_id, post_id FROM comments WHERE post_id=?", r.GetId())
 
 	// TODO: return proper errors depending on results of previous code
 
 	if err != nil {
 		log.Println(err)
-		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+		return grpc.Errorf(codes.Unknown, "Ouch!")
 	}
-	defer comments.Close()
+	defer cs.Close()
 
-	var Comments []*pb.Comment
-
-	for comments.Next() {
-		thisComment := &pb.Comment{}
-		comments.Scan(&thisComment.Author, &thisComment.Content, &thisComment.Created)
-		Comments = append(Comments, thisComment)
+	for cs.Next() {
+		c := &pb.Comment{}
+		cs.Scan(&c.Id, &c.Content, &c.Created, &c.LastEdited, &c.UserId, &c.PostId)
+		err := stream.Send(c)
+		if err != nil {
+			return err
+		}
 	}
 
-	if err = comments.Err(); err != nil {
+	if err = cs.Err(); err != nil {
+		log.Println(err)
+		return grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return nil
+}
+
+func (s *cmsServer) CreateComment(ctx context.Context, r *pb.CreateCommentRequest) (*pb.CommentRequest, error) {
+	res, err := database.Exec("INSERT INTO comments SET content=?, user_id=?, post_id=?", r.GetContent(), r.GetUserId(), r.GetPostId())
+
+	if err != nil {
 		log.Println(err)
 		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
 	}
 
-	return &pb.Comments{Comments}, nil
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &pb.CommentRequest{uint32(id)}, nil
+}
+
+func (s *cmsServer) CreateUser(ctx context.Context, r *pb.CreateUserRequest) (*pb.UserRequest, error) {
+	_, err := database.Exec("INSERT INTO users SET id=?, email=?", r.GetId(), r.GetEmail())
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &pb.UserRequest{r.GetId()}, nil
+}
+
+func (s *cmsServer) DeleteComment(ctx context.Context, r *pb.CommentRequest) (*empty.Empty, error) {
+	_, err := database.Exec("DELETE FROM comments WHERE id=?", r.GetId())
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (s *cmsServer) DeleteUser(ctx context.Context, r *pb.UserRequest) (*empty.Empty, error) {
+	_, err := database.Exec("DELETE FROM users WHERE id=?", r.GetId())
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (s *cmsServer) GetComment(ctx context.Context, r *pb.CommentRequest) (*pb.Comment, error) {
+	c := &pb.Comment{}
+
+	err := database.QueryRow("SELECT id, content, created, last_edited, user_id, post_id FROM comments WHERE id=?", r.GetId()).Scan(&c.Id, &c.Content, &c.Created, &c.LastEdited, &c.UserId, &c.PostId)
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return c, nil
+}
+
+func (s *cmsServer) GetComments(_ *empty.Empty, stream pb.Cms_GetCommentsServer) error {
+	cs, err := database.Query("SELECT id, content, created, last_edited, user_id, post_id FROM comments")
+
+	// TODO: return proper errors depending on results of previous code
+
+	if err != nil {
+		log.Println(err)
+		return grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+	defer cs.Close()
+
+	for cs.Next() {
+		c := &pb.Comment{}
+		cs.Scan(&c.Id, &c.Content, &c.Created, &c.LastEdited, &c.UserId, &c.PostId)
+		err := stream.Send(c)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err = cs.Err(); err != nil {
+		log.Println(err)
+		return grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return nil
+}
+
+func (s *cmsServer) GetPosts(_ *empty.Empty, stream pb.Cms_GetPostsServer) error {
+	ps, err := database.Query("SELECT id, title, content, created, last_edited FROM posts")
+
+	// TODO: return proper errors depending on results of previous code
+
+	if err != nil {
+		log.Println(err)
+		return grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+	defer ps.Close()
+
+	for ps.Next() {
+		p := &pb.Post{}
+		ps.Scan(&p.Id, &p.Title, &p.Content, &p.Created, &p.LastEdited)
+		err := stream.Send(p)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err = ps.Err(); err != nil {
+		log.Println(err)
+		return grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return nil
+}
+
+func (s *cmsServer) GetUser(ctx context.Context, r *pb.UserRequest) (*pb.User, error) {
+	u := &pb.User{}
+
+	err := database.QueryRow("SELECT id, email, created, last_active FROM users WHERE id=?", r.GetId()).Scan(&u.Id, &u.Email, &u.Created, &u.LastActive)
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return u, nil
+
+}
+
+func (s *cmsServer) GetUserComments(r *pb.UserRequest, stream pb.Cms_GetUserCommentsServer) error {
+	cs, err := database.Query("SELECT id, content, created, last_edited, user_id, post_id FROM comments WHERE user_id=?", r.GetId())
+
+	// TODO: return proper errors depending on results of previous code
+
+	if err != nil {
+		log.Println(err)
+		return grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+	defer cs.Close()
+
+	for cs.Next() {
+		c := &pb.Comment{}
+		cs.Scan(&c.Id, &c.Content, &c.Created, &c.LastEdited, &c.UserId, &c.PostId)
+		err := stream.Send(c)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err = cs.Err(); err != nil {
+		log.Println(err)
+		return grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return nil
+}
+
+func (s *cmsServer) PublishPost(ctx context.Context, r *pb.PostRequest) (*empty.Empty, error) {
+	_, err := database.Exec("UPDATE posts SET published=true WHERE id=?", r.GetId())
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (s *cmsServer) UnPublishPost(ctx context.Context, r *pb.PostRequest) (*empty.Empty, error) {
+	_, err := database.Exec("UPDATE posts SET published=false WHERE id=?", r.GetId())
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (s *cmsServer) UpdateComment(ctx context.Context, r *pb.UpdateCommentRequest) (*empty.Empty, error) {
+	_, err := database.Exec("UPDATE comments SET content=? WHERE id=?", r.GetContent(), r.GetId())
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (s *cmsServer) UpdatePost(ctx context.Context, r *pb.UpdatePostRequest) (*empty.Empty, error) {
+	_, err := database.Exec("UPDATE posts SET title=?, content=? WHERE id=?", r.GetTitle(), r.GetContent(), r.GetId())
+
+	if err != nil {
+		log.Println(err)
+		return nil, grpc.Errorf(codes.Unknown, "Ouch!")
+	}
+
+	return &empty.Empty{}, nil
 }
 
 func main() {
