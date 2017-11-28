271d5624572cd973133e2629a56cd46ec1ed6403
thurt <taylor.a.hurt@gmail.com>
2017-11-28 16:36:49 +0000

    updates proto validators, ensures request string fields have length limit

--- cms/proto/cms.validator.pb.go
+++ cms/proto/cms.validator.pb.go
@@ -50,11 +50,29 @@ func (this *PostRequest) Validate() error {
 	return nil
 }
 func (this *CreatePostRequest) Validate() error {
+	if !(len(this.Title) < 256) {
+		return go_proto_validators.FieldError("Title", fmt.Errorf(`value '%v' must length be less than '256'`, this.Title))
+	}
+	if !(len(this.Content) < 16777216) {
+		return go_proto_validators.FieldError("Content", fmt.Errorf(`value '%v' must length be less than '16777216'`, this.Content))
+	}
+	if !(len(this.Slug) == 0) {
+		return go_proto_validators.FieldError("Slug", fmt.Errorf(`Do not include a slug value in your request. This field is only used internally by the server.`))
+	}
 	return nil
 }
 func (this *UpdatePostRequest) Validate() error {
 	if !(this.Id > 0) {
 		return go_proto_validators.FieldError("Id", fmt.Errorf(`value '%v' must be greater than '0'`, this.Id))
+	}
+	if !(len(this.Title) < 256) {
+		return go_proto_validators.FieldError("Title", fmt.Errorf(`value '%v' must length be less than '256'`, this.Title))
+	}
+	if !(len(this.Content) < 16777216) {
+		return go_proto_validators.FieldError("Content", fmt.Errorf(`value '%v' must length be less than '16777216'`, this.Content))
+	}
+	if !(len(this.Slug) == 0) {
+		return go_proto_validators.FieldError("Slug", fmt.Errorf(`Do not include a slug value in your request. This field is only used internally by the server.`))
 	}
 	return nil
 }
@@ -67,15 +85,27 @@ func (this *CommentRequest) Validate() error {
 	}
 	return nil
 }
+
+var _regex_CreateCommentRequest_UserId = regexp.MustCompile("^[a-zA-Z]{3,18}$")
+
 func (this *CreateCommentRequest) Validate() error {
 	if this.Content == "" {
 		return go_proto_validators.FieldError("Content", fmt.Errorf(`value '%v' must not be an empty string`, this.Content))
 	}
+	if !(len(this.Content) < 65536) {
+		return go_proto_validators.FieldError("Content", fmt.Errorf(`value '%v' must length be less than '65536'`, this.Content))
+	}
+	if !_regex_CreateCommentRequest_UserId.MatchString(this.UserId) {
+		return go_proto_validators.FieldError("UserId", fmt.Errorf(`User Id must be 3-18 characters and can only include letters`))
+	}
 	if this.UserId == "" {
-		return go_proto_validators.FieldError("UserId", fmt.Errorf(`value '%v' must not be an empty string`, this.UserId))
+		return go_proto_validators.FieldError("UserId", fmt.Errorf(`User Id must be 3-18 characters and can only include letters`))
 	}
 	if this.PostId == "" {
 		return go_proto_validators.FieldError("PostId", fmt.Errorf(`value '%v' must not be an empty string`, this.PostId))
+	}
+	if !(len(this.PostId) < 37) {
+		return go_proto_validators.FieldError("PostId", fmt.Errorf(`value '%v' must length be less than '37'`, this.PostId))
 	}
 	return nil
 }
@@ -86,38 +116,47 @@ func (this *UpdateCommentRequest) Validate() error {
 	if this.Content == "" {
 		return go_proto_validators.FieldError("Content", fmt.Errorf(`value '%v' must not be an empty string`, this.Content))
 	}
+	if !(len(this.Content) < 65536) {
+		return go_proto_validators.FieldError("Content", fmt.Errorf(`value '%v' must length be less than '65536'`, this.Content))
+	}
 	return nil
 }
 func (this *User) Validate() error {
 	return nil
 }
 
-var _regex_UserRequest_Id = regexp.MustCompile("^[a-zA-Z]{3,}$")
+var _regex_UserRequest_Id = regexp.MustCompile("^[a-zA-Z]{3,18}$")
 
 func (this *UserRequest) Validate() error {
 	if !_regex_UserRequest_Id.MatchString(this.Id) {
-		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be at least 3 characters and can only include letters`))
+		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be 3-18 characters and can only include letters`))
 	}
 	if this.Id == "" {
-		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be at least 3 characters and can only include letters`))
+		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be 3-18 characters and can only include letters`))
 	}
 	return nil
 }
 
-var _regex_CreateUserRequest_Id = regexp.MustCompile("^[a-zA-Z]{3,}$")
+var _regex_CreateUserRequest_Id = regexp.MustCompile("^[a-zA-Z]{3,18}$")
 
 func (this *CreateUserRequest) Validate() error {
 	if !_regex_CreateUserRequest_Id.MatchString(this.Id) {
-		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be at least 3 characters and can only include letters`))
+		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be 3-18 characters and can only include letters`))
 	}
 	if this.Id == "" {
-		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be at least 3 characters and can only include letters`))
+		return go_proto_validators.FieldError("Id", fmt.Errorf(`User Id must be 3-18 characters and can only include letters`))
 	}
 	if this.Email == "" {
 		return go_proto_validators.FieldError("Email", fmt.Errorf(`value '%v' must not be an empty string`, this.Email))
 	}
+	if !(len(this.Email) < 256) {
+		return go_proto_validators.FieldError("Email", fmt.Errorf(`value '%v' must length be less than '256'`, this.Email))
+	}
 	if this.Password == "" {
 		return go_proto_validators.FieldError("Password", fmt.Errorf(`value '%v' must not be an empty string`, this.Password))
+	}
+	if !(len(this.Password) < 51) {
+		return go_proto_validators.FieldError("Password", fmt.Errorf(`value '%v' must length be less than '51'`, this.Password))
 	}
 	return nil
 }
