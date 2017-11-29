ea08cebb1f2a48a7339c18b856fc404cdb153827
thurt <taylor.a.hurt@gmail.com>
2017-11-29 18:03:26 +0000

    simplifies switch syntax

--- cms/mysqlprovider/mysqlprovider.go
+++ cms/mysqlprovider/mysqlprovider.go
@@ -52,17 +52,13 @@ func sqlErrorToGrpcError(err error) error {
 		// i'm not familiar with mysql errors but i suspect there will be more errors that come up commonly
 
 		switch deviceErr.Number {
-		case mysqlerr.ER_DUP_ENTRY: // tried to insert primary key value that already exists
-			fallthrough
-		case mysqlerr.ER_TRUNCATED_WRONG_VALUE_FOR_FIELD: // tried to insert/update a value with an incorrect type
-			fallthrough
-		case mysqlerr.ER_DATA_TOO_LONG: // tried to insert string that is too long
-			fallthrough
-		case mysqlerr.ER_NO_DEFAULT_FOR_FIELD: // tried to insert row without passing a required value
-			fallthrough
-		case mysqlerr.ER_ROW_IS_REFERENCED_2: // tried to update/delete a row key that is still referenced as a foreign key in another tabele
-			fallthrough
-		case mysqlerr.ER_NO_REFERENCED_ROW_2: // tried to supply a foreign key value that is not found in parent table
+		case
+			mysqlerr.ER_DUP_ENTRY,                       // tried to insert primary key value that already exists
+			mysqlerr.ER_TRUNCATED_WRONG_VALUE_FOR_FIELD, // tried to insert/update a value with an incorrect type
+			mysqlerr.ER_DATA_TOO_LONG,                   // tried to insert string that is too long
+			mysqlerr.ER_NO_DEFAULT_FOR_FIELD,            // tried to insert row without passing a required value
+			mysqlerr.ER_ROW_IS_REFERENCED_2,             // tried to update/delete a row key that is still referenced as a foreign key in another tabele
+			mysqlerr.ER_NO_REFERENCED_ROW_2:             // tried to supply a foreign key value that is not found in parent table
 			e = status.Error(codes.InvalidArgument, err.Error())
 		case mysqlerr.ER_PARSE_ERROR: // tried to execute a sql statement that has syntax error(s)
 			e = status.Error(codes.Internal, err.Error())
