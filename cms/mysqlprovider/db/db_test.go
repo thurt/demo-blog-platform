22bffb6a8d02731e2f90d258d0483e817df30f70
thurt <taylor.a.hurt@gmail.com>
2017-12-12 21:18:04 +0000

    updates checkSyntax tests

--- cms/mysqlprovider/db/db_test.go
+++ cms/mysqlprovider/db/db_test.go
@@ -4,8 +4,12 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/thurt/demo-blog-platform/cms/mysqlprovider"
 	"github.com/thurt/demo-blog-platform/cms/mysqlprovider_internal"
+	pb "github.com/thurt/demo-blog-platform/cms/proto"
+
+	"github.com/google/gofuzz"
 	"github.com/xwb1989/sqlparser"
 )
 
@@ -18,7 +22,7 @@ func checkSyntax(sql string, t *testing.T, strict bool) {
 	}
 
 	if err != nil {
-		t.Error(err, sql)
+		t.Error(t.Name(), err, sql)
 	}
 }
 
@@ -43,25 +47,43 @@ func TestCreateTablesSQLSyntax(t *testing.T) {
 }
 
 func TestRequestSQLSyntax(t *testing.T) {
-	q := mysqlprovider.NewSqlQuery()
-	q_internal := mysqlprovider_internal.NewSqlQuery()
+	q := &mysqlprovider.SqlQuery{}
+	q_internal := mysqlprovider_internal.SqlQuery{}
+	f := fuzz.New()
+
+	ccr := &pb.CreateCommentRequest{}
+	f.Fuzz(ccr)
+	cpr := &pb.CreatePostRequest{}
+	f.Fuzz(cpr)
+	cur := &pb.CreateUserRequest{}
+	f.Fuzz(cur)
+	cr := &pb.CommentRequest{}
+	f.Fuzz(cr)
+	pr := &pb.PostRequest{}
+	f.Fuzz(pr)
+	ur := &pb.UserRequest{}
+	f.Fuzz(ur)
+	ucr := &pb.UpdateCommentRequest{}
+	f.Fuzz(ucr)
+	upr := &pb.UpdatePostRequest{}
+	f.Fuzz(upr)
 
 	checkSyntax(q_internal.GetUserPassword(), t, true)
-	checkSyntax(q.CreateComment(), t, true)
-	checkSyntax(q.CreatePost(), t, true)
-	checkSyntax(q.CreateUser(), t, true)
-	checkSyntax(q.DeleteComment(), t, true)
-	checkSyntax(q.DeletePost(), t, true)
-	checkSyntax(q.DeleteUser(), t, true)
-	checkSyntax(q.GetComment(), t, true)
+	checkSyntax(q.CreateComment(ccr), t, true)
+	checkSyntax(q.CreatePost(cpr), t, true)
+	checkSyntax(q.CreateUser(cur), t, true)
+	checkSyntax(q.DeleteComment(cr), t, true)
+	checkSyntax(q.DeletePost(pr), t, true)
+	checkSyntax(q.DeleteUser(ur), t, true)
+	checkSyntax(q.GetComment(cr), t, true)
 	checkSyntax(q.GetComments(), t, true)
-	checkSyntax(q.GetPost(), t, true)
-	checkSyntax(q.GetPosts(), t, true)
-	checkSyntax(q.GetPostComments(), t, true)
-	checkSyntax(q.GetUser(), t, true)
-	checkSyntax(q.GetUserComments(), t, true)
-	checkSyntax(q.PublishPost(), t, true)
-	checkSyntax(q.UnPublishPost(), t, true)
-	checkSyntax(q.UpdateComment(), t, true)
-	checkSyntax(q.UpdatePost(), t, true)
+	checkSyntax(q.GetPost(pr), t, true)
+	checkSyntax(q.GetPosts(&empty.Empty{}), t, true)
+	checkSyntax(q.GetPostComments(pr), t, true)
+	checkSyntax(q.GetUser(ur), t, true)
+	checkSyntax(q.GetUserComments(ur), t, true)
+	checkSyntax(q.PublishPost(pr), t, true)
+	checkSyntax(q.UnPublishPost(pr), t, true)
+	checkSyntax(q.UpdateComment(ucr), t, true)
+	checkSyntax(q.UpdatePost(upr), t, true)
 }
