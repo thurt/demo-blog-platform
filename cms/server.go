719020d56784964c8eb6b105df2249085e2760a9
thurt <taylor.a.hurt@gmail.com>
2017-12-16 17:57:15 +0000

    imports mysql driver into server.go

--- cms/server.go
+++ cms/server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"github.com/grpc-ecosystem/go-grpc-middleware/validator"
