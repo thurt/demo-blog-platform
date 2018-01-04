0c3ee4a28bcd170539f3b9d39f38fcf6d421bf2b
thurt <taylor.a.hurt@gmail.com>
2018-01-04 20:07:53 +0000

    adds to Provider iface AdminExists and modifies CreateUser signature

--- cms/domain/domain.go
+++ cms/domain/domain.go
@@ -4,32 +4,28 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	pb "github.com/thurt/demo-blog-platform/cms/proto"
 )
 
 type Provider interface {
-	// Post CRUD
 	CreatePost(context.Context, *pb.CreatePostRequest) (*pb.PostRequest, error)
 	GetPost(context.Context, *pb.PostRequest) (*pb.Post, error)
 	UpdatePost(context.Context, *pb.UpdatePostRequest) (*empty.Empty, error)
 	DeletePost(context.Context, *pb.PostRequest) (*empty.Empty, error)
-	// Post Use-Cases
 	GetPostComments(*pb.PostRequest, pb.Cms_GetPostCommentsServer) error
 	GetPosts(*empty.Empty, pb.Cms_GetPostsServer) error
 	PublishPost(context.Context, *pb.PostRequest) (*empty.Empty, error)
 	UnPublishPost(context.Context, *pb.PostRequest) (*empty.Empty, error)
-	// User CRD
-	CreateUser(context.Context, *pb.CreateUserRequest) (*pb.UserRequest, error)
+	CreateUser(context.Context, *pb.CreateUserWithRole) (*pb.UserRequest, error)
 	GetUser(context.Context, *pb.UserRequest) (*pb.User, error)
+	AdminExists(context.Context, *empty.Empty) (*wrappers.BoolValue, error)
 	DeleteUser(context.Context, *pb.UserRequest) (*empty.Empty, error)
-	// User Use-Cases
 	GetUserComments(*pb.UserRequest, pb.Cms_GetUserCommentsServer) error
-	// Comment CRUD
 	CreateComment(context.Context, *pb.CreateCommentRequest) (*pb.CommentRequest, error)
 	GetComment(context.Context, *pb.CommentRequest) (*pb.Comment, error)
 	UpdateComment(context.Context, *pb.UpdateCommentRequest) (*empty.Empty, error)
 	DeleteComment(context.Context, *pb.CommentRequest) (*empty.Empty, error)
-	// Comment Use-Cases
 	GetComments(*empty.Empty, pb.Cms_GetCommentsServer) error
 }
 
