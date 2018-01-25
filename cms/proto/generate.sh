8fcaddc4ec83a6557289800268ba48927b1ddcfd
thurt <taylor.a.hurt@gmail.com>
2018-01-25 19:13:42 +0000

    changes proto generate code to move cms.swagger.json up one dir level

--- cms/proto/generate.sh
+++ cms/proto/generate.sh
@@ -35,7 +35,7 @@ protoc \
     -I. \
     -I$GOPATH/src \
     -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-     --swagger_out=logtostderr=true:. \
+     --swagger_out=logtostderr=true:../ \
      ${SERVICE} && \
 
 #generate Cms mocks
