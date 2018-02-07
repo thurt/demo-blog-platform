155d54fb9dd4a82ff37fabdbb85df388c15e74ff
thurt <taylor.a.hurt@gmail.com>
2018-02-07 17:05:18 +0000

    adds forwarder_overwrite.got which includes basic cache-control and etag verification for some endpoints

---
+++ cms/proto/forwarder_overwrite.go
@@ -0,0 +1,77 @@
+package cms
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+)
+
+func maybeRespondNotModified(req *http.Request, w http.ResponseWriter, etag string) bool {
+	// create an etag
+	wetag := fmt.Sprintf("W/%q", etag)
+	w.Header().Add("ETag", wetag)
+
+	// check whether the request provides an etag
+	inm := req.Header.Get("If-None-Match")
+	if inm == wetag {
+		w.WriteHeader(http.StatusNotModified)
+		return true
+	}
+	return false
+}
+
+func GetPost(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) {
+	// add cache-control rules for this proxy endpoint
+	w.Header().Add("Cache-Control", "no-cache,private")
+	if maybeRespondNotModified(req, w, resp.(*Post).GetLastEdited()) == false {
+		runtime.ForwardResponseMessage(ctx, mux, marshaler, w, req, resp, opts...)
+	}
+}
+
+func GetUser(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) {
+	// add cache-control rules for this proxy endpoint
+	w.Header().Add("Cache-Control", "public,max-age=60")
+	if maybeRespondNotModified(req, w, resp.(*User).GetLastActive()) == false {
+		runtime.ForwardResponseMessage(ctx, mux, marshaler, w, req, resp, opts...)
+	}
+}
+
+func GetComment(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) {
+	// add cache-control rules for this proxy endpoint
+	w.Header().Add("Cache-Control", "public,max-age=60")
+	if maybeRespondNotModified(req, w, resp.(*Comment).GetLastEdited()) == false {
+		runtime.ForwardResponseMessage(ctx, mux, marshaler, w, req, resp, opts...)
+	}
+}
+
+func AuthUser(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) {
+	// add cache-control rules for this proxy endpoint
+	w.Header().Add("Cache-Control", "no-store")
+	runtime.ForwardResponseMessage(ctx, mux, marshaler, w, req, resp, opts...)
+}
+
+func Setup(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) {
+	// add cache-control rules for this proxy endpoint
+	w.Header().Add("Cache-Control", "no-store")
+	runtime.ForwardResponseMessage(ctx, mux, marshaler, w, req, resp, opts...)
+}
+
+func IsSetup(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) {
+	// add cache-control rules for this proxy endpoint
+	w.Header().Add("Cache-Control", "no-store")
+	runtime.ForwardResponseMessage(ctx, mux, marshaler, w, req, resp, opts...)
+}
+
+func init() {
+	forward_Cms_GetPost_0 = GetPost
+	forward_Cms_GetPostBySlug_0 = GetPost
+	forward_Cms_GetUser_0 = GetUser
+	forward_Cms_GetComment_0 = GetComment
+
+	forward_Cms_AuthUser_0 = AuthUser
+	forward_Cms_Setup_0 = Setup
+	forward_Cms_IsSetup_0 = IsSetup
+}
