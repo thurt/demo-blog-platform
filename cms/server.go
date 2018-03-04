550fcd97a457d9ae58b1c8338411212d4185f268
thurt <taylor.a.hurt@gmail.com>
2018-03-04 01:30:05 +0000

    updates server.go to pass a new hasher to usecases

--- cms/server.go
+++ cms/server.go
@@ -16,6 +16,7 @@ import (
 	"github.com/memcachier/mc"
 	"github.com/thurt/demo-blog-platform/cms/authentication"
 	"github.com/thurt/demo-blog-platform/cms/authorization"
+	"github.com/thurt/demo-blog-platform/cms/hasher"
 	"github.com/thurt/demo-blog-platform/cms/mysqlprovider"
 	pb "github.com/thurt/demo-blog-platform/cms/proto"
 	"github.com/thurt/demo-blog-platform/cms/usecases"
@@ -88,7 +89,7 @@ func main() {
 		)),
 	}
 	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterCmsServer(grpcServer, authorization.New(usecases.New(mysqlprovider.New(db), authProvider)))
+	pb.RegisterCmsServer(grpcServer, authorization.New(usecases.New(mysqlprovider.New(db), authProvider, hasher.New())))
 	log.Printf("Started grpc server on port %d", PORT)
 
 	// setup rest proxy server
