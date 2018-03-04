5bbe3e81e32bf7b0f10e2d3ca2bfac8122fe0007
thurt <taylor.a.hurt@gmail.com>
2018-03-04 15:57:23 +0000

    adds package implementation of Emailer service

---
+++ cms/emailer/emailer.go
@@ -0,0 +1,46 @@
+package emailer
+
+import (
+	"bytes"
+	"net/smtp"
+
+	"golang.org/x/net/context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	pb "github.com/thurt/demo-blog-platform/cms/proto"
+)
+
+type emailer struct {
+	c *smtp.Client
+}
+
+func New() pb.EmailerServer {
+	return &emailer{}
+}
+
+func (e *emailer) Send(ctx context.Context, email *pb.Email) (*empty.Empty, error) {
+	// Set the sender and recipient.
+	e.c.Mail(email.GetFrom())
+	e.c.Rcpt(email.GetTo())
+	// Send the email body.
+	// TODO: I think there should be a mutex on m.c because m.c.Data() writer is supposed to be Closed before more commands are issued to m.c
+	wc, err := e.c.Data()
+	defer wc.Close()
+	if err != nil {
+		return nil, err
+	}
+	msg := "From: " + email.GetFrom() + "\n" +
+		"To: " + email.GetTo() + "\n" +
+		"Subject: " + email.GetSubject() + "\n\n" +
+		email.GetBody()
+
+	buf := bytes.NewBufferString(msg)
+	if _, err = buf.WriteTo(wc); err != nil {
+		return nil, err
+	}
+	return &empty.Empty{}, nil
+}
+
+//	err := smtp.SendMail("mail:25",
+//		smtp.PlainAuth("", from, pass, "mail"),
+//		from, []string{to}, []byte(msg))
